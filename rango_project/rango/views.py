d7d4253cc535826217e06d5d76a0027d6a55f3ee
Mitchell Baxter <2193433B@student.gla.ac.uk>
2017-01-26 16:01:28 +0000

    Fixed some of the automated test cases.

--- rango_project/rango/views.py
+++ rango_project/rango/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 	
 def about(request):
-    context_dict = {'aboutmessage': "This is a handy about page!"}
+    context_dict = {'aboutmessage': "This is a handy about page! This tutorial has been put together by 2193433B"}
     return render(request, 'rango/about.html', context=context_dict)
