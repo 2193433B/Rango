a01b2516b8c4285b04014afdbf4dd8aacede011b
Mitchell Baxter <2193433B@student.gla.ac.uk>
2017-01-26 14:36:22 +0000

    Finished ch5.7.

--- rango_project/populate_rango.py
+++ rango_project/populate_rango.py
@@ -1,10 +1,14 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','rango_project.settings')
 
 import django
 django.setup()
 
 from rango.models import Category, Page
+
+
+
+
 
 def populate():
 	python_pages = [
